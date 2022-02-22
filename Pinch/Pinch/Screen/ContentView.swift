02a5c9c2629fa1ba866ffd5fd3a92a90a35196e5
sky <anvanc37@gmail.com>
2022-02-21 22:42:09 -0500

    add AppIcon

---
+++ Pinch/Pinch/Screen/ContentView.swift
@@ -0,0 +1,27 @@
+//
+//  ContentView.swift
+//  Pinch
+//
+//  Created by sky on 2/6/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    // MARK: - PROPERTY
+    
+    // MARK: - FUNCTION
+    
+    // MARK: - CONTENT
+    
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
